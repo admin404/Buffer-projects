8d46a424b73741bb32bcc73bcb7ca67a261279f3
ScoutV <neterror404@gmail.com>
2021-02-11 11:54:55 +0200

    update

--- python _apps/test_pyside_app/main.py
+++ python _apps/test_pyside_app/main.py
@@ -4,13 +4,14 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
-class main():
+class main(QMainWindow):
     def __init__(self):
         super(main, self).__init__()
         
-        wid = widget()
+        self.wid = widget()
+        self.setCentralWidget(self.wid)
 
-class widget():
+class widget(QWidget):
     def __init__(self):
         super(widget,self).__init__()
         self.init()
